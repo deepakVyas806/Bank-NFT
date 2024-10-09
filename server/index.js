4d7fb90c7bc3fd49eac8250f9fe838373c5d57cb
unknown <adityaparasharadiadi12@gmail.com>
2024-10-09 20:01:54 +0530

    newe changes

--- server/index.js
+++ server/index.js
@@ -8,14 +8,10 @@ const PORT = process.env.PORT || 4000 ;
 const app = express();
 
 app.get('/',(req,res)=>{
-<<<<<<< HEAD
+
    res.status(200).json({success:true, message:"Hey-SNOOK-CODERS"})
-=======
-
-   res.status(200).json({success:true, message:"SNOOK-CODERS"})
-
->>>>>>> a0af7200d4884619e1974eb901487820ecca1007
 })
+
 
 
 app.listen(PORT,()=>{
