8929f4f58251018548eacf3f9bc1a3831674890c
unknown <adityaparasharadiadi12@gmail.com>
2024-10-09 18:29:40 +0530

    add

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.get('/',(req,res)=>{
 
-   res.status(200).json({success:true, message:"SNOOK-CODERS-MAIN_NEW"})
+   res.status(200).json({success:true, message:"SNOOK-CODERS-DEV_NEW"})
 
 })
 
