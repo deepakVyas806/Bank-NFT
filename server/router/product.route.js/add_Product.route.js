c2b721ed172a9b7fd53b6e049d6ea9f272dc2744
unknown <adityaparasharadiadi12@gmail.com>
2024-10-22 00:45:54 +0530

    changes are done

--- server/router/product.route.js/add_Product.route.js
+++ server/router/product.route.js/add_Product.route.js
@@ -2,6 +2,7 @@ import express from "express";
 import { addProduct } from "../../controller/product.controller.js/addProduct.controller.js"; // Make sure this is the correct path
 import { verifyToken } from "../../middleware/verifyToken.js";
 import { upload } from "../../middleware/multer.middleware.js";
+import { get_product } from "../../controller/product.controller.js/general.controller.js";
 
 const proute = express.Router();
 
@@ -12,5 +13,7 @@ proute.post(
   upload.single("product_image"),
   addProduct
 );
+
+proute.get('/get-products',get_product)
 
 export { proute };
