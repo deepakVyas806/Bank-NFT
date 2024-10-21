f5ef6c2c7fa5d18c4088945f5f3008305bf83798
@snookcoder <126566535+snookCoder@users.noreply.github.com>
2024-10-21 12:35:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(
 app.use(cookieParser()); // Parse cookies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true })); // Parse form data
-// app.use("/uploads", express.static("uploads"));
+app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
   console.log("Request Cookies:", req.cookies); // Log the cookies
