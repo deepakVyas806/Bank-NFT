3312a2d2c9deaff7c3ead516a4fc445071a0bdb8
@snookcoder <126566535+snookCoder@users.noreply.github.com>
2024-10-21 12:48:39 +0530

    Update multer.middleware.js

--- server/middleware/multer.middleware.js
+++ server/middleware/multer.middleware.js
@@ -1,30 +1,27 @@
 import dotenv from 'dotenv';
 import multer from 'multer';
-import path from 'path'
+import path from 'path';
 import { v2 as cloudinary } from 'cloudinary';
-
 
 // Load environment variables
 dotenv.config();
 
-// Configure Multer to use memory storage
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage: storage });
-
-//gio with the disak storage this time
-
+// Configure Multer to use disk storage
 const storage = multer.diskStorage({
-   
-    destination:function(req,file,cb){
-        cb(null,'uploads/')
+    destination: function (req, file, cb) {
+        // Log the destination where the file will be stored
+        console.log(`File will be stored in: uploads/`);
+        cb(null, 'uploads/');
     },
-    filename:function(req,file,cb){
-        cb(null,file.originalname)
+    filename: function (req, file, cb) {
+        // Log the original file name
+        console.log(`Original file name: ${file.originalname}`);
+        cb(null, file.originalname);
     }
-     
-})
+});
 
-const upload = multer({storage:storage}).single('product_image')
+// Create the upload middleware
+const upload = multer({ storage: storage }).single('product_image');
 
-
+// Export the upload middleware
 export { upload };
